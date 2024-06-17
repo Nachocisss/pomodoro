2936edcebe3ff52be7f85963e465750fced0e314
Ignacio Cisternas <nachocisternas95@gmail.com>
2024-06-16 23:18:44 -0400

    timer buttons

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,9 +1,56 @@
 import React from "react";
 import "./Timer.css";
+import { FaPlay, FaPause, FaStop, FaFastForward } from "react-icons/fa";
+
 export default function Timer() {
+  const buttonsInfo = [
+    {
+      icon: <FaPlay size={40} fill={"black"} />,
+      onclick: () => {
+        console.log("play");
+      },
+    },
+    {
+      icon: <FaPause size={40} fill={"black"} />,
+      onclick: () => {
+        console.log("pause");
+      },
+    },
+    {
+      icon: <FaStop size={40} fill={"black"} />,
+      onclick: () => {
+        console.log("play");
+      },
+    },
+    {
+      icon: <FaFastForward size={40} fill={"black"} />,
+      onclick: () => {
+        console.log("FaFastForward");
+      },
+    },
+  ];
+
+  function clockRender() {
+    return (
+      <div className="clockContainer">
+        <span className="clockTime">10:00</span>
+      </div>
+    );
+  }
+  function timerButtons() {
+    return (
+      <div className="timerButtons">
+        {buttonsInfo.map((b) => {
+          return <div className="timerIconContainer">{b.icon}</div>;
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="timerContainer">
-      <span>Timer</span>
+      {clockRender()}
+      {timerButtons()}
     </div>
   );
 }
